572669be6c60a49d0bd1688d8cca450f5cd64664
Raphael <raphaelraia@outlook.com.br>
2022-10-26 21:26:03 -0300

    first commit

---
+++ src/assets/styles/themes/default.js
@@ -0,0 +1,15 @@
+export default {
+  colors: {
+    background: '#F6F5FC',
+    primary: {
+      lighter: '#E0E3FF',
+      light: '#6674F4',
+      main: '#5061fc',
+      dark: '#3346F0',
+    },
+    gray: {
+      900: '#222',
+      200: '#BCBCBC',
+    },
+  },
+};
